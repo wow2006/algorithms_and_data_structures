a49340b507e7f1b4f4b12d866b7f51c852a4ff97
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2019-03-20 18:05:40 +0200

    add testNthToLastNode

--- LinkedList/tests/testNthToLastNode.cpp
+++ LinkedList/tests/testNthToLastNode.cpp
@@ -1,1 +1,32 @@
+#include <gtest/gtest.h>
+/*
+ * Program:
+ * -------
+ *  Create Linked list store 1 -> 2 -> 3 -> 4
+ *  get nth node from last node
+ *
+ */
+
+struct LinkedList {
+    struct Node {};
+
+    void add(int value) {
+        m_pHead = new Node;
+    }
+
+    Node *m_pHead = nullptr;
+};
+
+TEST(LinkedListCreate, HeadIsNullptr) {
+    LinkedList linkedList;
+
+    EXPECT_EQ(nullptr, linkedList.m_pHead);
+}
+
+TEST(LinkedListCreate, CreateOneNode) {
+    LinkedList linkedList;
+    linkedList.add(1);
+
+    EXPECT_NE(nullptr, linkedList.m_pHead);
+}
 
