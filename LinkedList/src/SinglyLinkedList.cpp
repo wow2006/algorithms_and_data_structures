dc5f56189b8e84c0bd0e1713225ffcbce82e3691
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2019-06-12 13:05:22 +0200

    Add simple initlizer list

--- LinkedList/src/SinglyLinkedList.cpp
+++ LinkedList/src/SinglyLinkedList.cpp
@@ -2,11 +2,19 @@
 
 LinkedList::LinkedList() = default;
 
-LinkedList::LinkedList(const std::initializer_list<int>& l)
-  : m_pRoot{new Node{*l.begin()}} {}
-  
-LinkedList::~LinkedList() {
-  delete m_pRoot;
-  m_pRoot = nullptr;
+LinkedList::LinkedList(const std::initializer_list<int> &l)
+    : m_pRoot{new Node{*l.begin()}} {
+  if (l.size() >= 2) {
+    m_pRoot->m_pNext = new Node{*(l.begin() + 1)};
+  }
 }
 
+LinkedList::~LinkedList() {
+  if(m_pRoot != nullptr) {
+    delete m_pRoot->m_pNext;
+    m_pRoot->m_pNext = nullptr;
+
+    delete m_pRoot;
+    m_pRoot = nullptr;
+  }
+}
--- LinkedList/tests/testSinglyLinkedList.cpp
+++ LinkedList/tests/testSinglyLinkedList.cpp
@@ -9,8 +9,20 @@ TEST(SinglyLinkedList, emptyListIsNull) {
 }
 
 TEST(SinglyLinkedList, initializeFromInitializationList) {
-  LinkedList list{1, 2, 3, 4};
+  LinkedList list{1};
 
   EXPECT_FALSE(list.m_pRoot == nullptr);
   EXPECT_EQ(1, list.m_pRoot->m_iValue);
+
+  EXPECT_TRUE(list.m_pRoot->m_pNext == nullptr);
 }
+
+TEST(SinglyLinkedList, initializeFromInitializationList2) {
+  LinkedList list{1, 2};
+
+  EXPECT_FALSE(list.m_pRoot == nullptr);
+  EXPECT_EQ(1, list.m_pRoot->m_iValue);
+
+  EXPECT_FALSE(list.m_pRoot->m_pNext == nullptr);
+}
+
